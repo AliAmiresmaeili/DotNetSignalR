d255465355a73e04e92488d42daf5c3cb09ab973
Ali Amiresmaeili <aliamiresmaeili@gmail.com>
2022-09-26 09:51:06 +0330

    add services & middleware

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,15 +1,24 @@
+using Api.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("ClientPermission", policy =>
+    {
+        policy.AllowAnyHeader()
+            .AllowAnyMethod()
+            .WithOrigins("http://localhost:3000")
+            .AllowCredentials();
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -21,5 +30,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseCors("ClientPermission");
+app.UseRouting();
+app.UseEndpoints(endpoints => endpoints.MapHub<ChatHub>("/hubs/chat"));
 
 app.Run();
